a367badd8e8bfbd5ea34ebf5cf3067b6b3320259
armasonix <74196072+armasonix@users.noreply.github.com>
2025-11-22 15:14:54 +0300

    clean: cleanup some files in project

--- include/utils/FPSCounter.h
+++ include/utils/FPSCounter.h
@@ -1,22 +1,28 @@
 #pragma once
 #include <SFML/System.hpp>
 
-class FPSCounter 
+namespace ark
 {
-public:
-    void frameStart() { /* noop */ }
-    void frameEnd() 
+
+class FPSCounter
     {
-        ++m_frames;
-        if (m_clock.getElapsedTime().asSeconds() >= 0.5f) 
+    public:
+        void frameStart() { /* noop */ }
+        void frameEnd()
         {
-            m_fps = static_cast<float>(m_frames) / m_clock.restart().asSeconds();
-            m_frames = 0;
+            ++m_frames;
+            if (m_clock.getElapsedTime().asSeconds() >= 0.5f)
+            {
+                m_fps = static_cast<float>(m_frames) / m_clock.restart().asSeconds();
+                m_frames = 0;
+            }
         }
-    }
-    float fps() const { return m_fps; }
-private:
-    sf::Clock m_clock;
-    int m_frames{ 0 };
-    float m_fps{ 0.f };
-};
+        float fps() const { return m_fps; }
+
+    private:
+        sf::Clock m_clock;
+        int m_frames{ 0 };
+        float m_fps{ 0.f };
+    };
+
+} // namespace ark
